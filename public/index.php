3c6f7ae8f380c850d07526e0e8f36de254f11f25
codyngpriest <vilhopriestly@gmail.com>
2024-01-02 08:05:03 +0000

    implement routes path and method

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
+
+include __DIR__ . "/../src/App/functions.php";
+
 $app = include __DIR__ . '/../src/App/bootstrap.php';
 
 $app->run();
+
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -1,12 +1,17 @@
 <?php
 
 declare(strict_types=1);
+include  "functions.php";
 
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 
 $app = new App;
+
+$app->get('/');
+
+dd($app);
 
 return $app;
 
