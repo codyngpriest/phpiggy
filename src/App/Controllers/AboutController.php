cdcdce9af3b77b2b761a5d9e6650dc5c1ee4c216
codyngpriest <vilhopriestly@gmail.com>
2024-01-04 08:44:55 +0000

    add about page

---
+++ src/App/Controllers/AboutController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
+class Aboutcontroller
+{
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(PATHS::VIEW);
+    }
+
+    public function about()
+    {
+        echo $this->view->render('about.php', [
+            'title' => 'About'
+        ]);
+    }
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -1,16 +1,16 @@
 <?php
 
 declare(strict_types=1);
-include  "functions.php";
+//include  "functions.php";
 
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 $app = new App;
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
-
